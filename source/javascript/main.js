4d61ec07c3c4bc70181d7586134e1b82af7b71e3
Miuki Miu <elizabet.oliveira@gmail.com>
2016-04-08 15:18:20 +0100

    Testing p5 and load it from cdn

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -23,12 +23,15 @@
 			playlist = ['dirty_south_loop_85bpm', 'pop_hiphop_loop_100bpm'],
 			dir = "audio/",
 			ext = ".mp3",
+			input,
+			analyzer,
 			currentTrack = 0,
 			seekslider,
 			seeking=false,
 			audio_context,
 			recorder,
-			seekto;
+			seekto,
+			yoff = 0.0;
 
 			pauseState.attr("display", "none");
 
@@ -42,11 +45,8 @@
 				this.play();
 			}, false);
 
-			//audio.addEventListener("timeupdate", function(){ timeUpdate(); });
-			//audio.addEventListener("loadedmetadata", function(){ timeDurUpdate(); });
-			audio.addEventListener("tracktitle", function(){ titleUpdate(); });
 
-			//audio.addEventListener("ended", function(){ nextTrack(); });
+			audio.addEventListener("tracktitle", function(){ titleUpdate(); });
 
 			// wheel animation left
 			function wheelAnimationL() {
@@ -339,3 +339,51 @@
 					__log('No live audio input: ' + e);
 				});
 			};
+
+			function setup() {
+			  createCanvas(710, 400);
+
+			  // Create an Audio input
+			  mic = new p5.AudioIn();
+
+			  // start the Audio Input.
+			  // By default, it does not .connect() (to the computer speakers)
+			  mic.start();
+			}
+
+			function draw() {
+			  background(200);
+
+				fill(555);
+
+			  // Get the overall volume (between 0 and 1.0)
+			  var vol = mic.getLevel();
+
+				console.log(vol);
+
+				background(51);
+
+				fill(255);
+				// We are going to draw a polygon out of the wave points
+				beginShape();
+
+				var xoff = 0;       // Option #1: 2D Noise
+
+				// Iterate over horizontal pixels
+				for (var x = 0; x <= width; x += 10) {
+					// Calculate a y value according to noise, map to
+
+					// Option #1: 2D Noise
+					var y = map(noise(xoff, yoff), 0, 1, 200,300);
+
+					// Set the vertex
+					vertex(x, y);
+					// Increment x dimension for noise
+					xoff += (0.05 + vol);
+				}
+				// increment y dimension for noise
+				yoff += (0.1 + vol);
+				vertex(width, height);
+				vertex(0, height);
+				endShape(CLOSE);
+			}
